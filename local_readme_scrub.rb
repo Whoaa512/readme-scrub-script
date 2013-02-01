81f8d15da87b100a100436ae2dc2c8c39155aafb
Whoaa512 <whoaa512@gmail.com>
2013-01-31 19:05:55 -0800

    First

---
+++ local_readme_scrub.rb
@@ -0,0 +1,14 @@
+
+def scrub_local_repo (*filenames)
+
+  raise ArgumentError, 'Arguments array cannot be empty' unless filenames.any?
+
+  # No need to do file sanity check--this command is tolerant of invalid filenames
+  command = "git filter-branch --index-filter 'git rm --cached --ignore-unmatch #{filenames.join(' ')}' --prune-empty --tag-name-filter cat -- --all"
+  %x(#{command})
+end
+
+
+
+# TODO: before scrubbing check to make sure current dir is a catalyst repo
+scrub_local_repo 'README.*'
